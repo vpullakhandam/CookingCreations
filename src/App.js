ffed0e25df7e51d8cb51dbee4a2bebf8757514c0
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-05-02 14:19:44 +0530

    added navbar.css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../assets/pizzaLogo.png";
-
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
         <img src={Logo} />
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   );
 }
