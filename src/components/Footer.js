ede99b46cf8e11adaf5a4d3daecf6543bedcdc13
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-05-02 17:34:57 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon />
+        <LinkedInIcon />
+        <XIcon />
+        <FacebookIcon />
+      </div>
+      <p>&copy; 2024 pizza</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
