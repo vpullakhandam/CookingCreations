43fb61b3820ae68c0941d14eba322635ebc5d5e3
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-05-04 20:58:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Router >
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
