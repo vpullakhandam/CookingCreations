0e4692eff51a30263e099d2b0b473d5392a87a8c
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-05-04 21:07:50 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} alt="Description of the image" />
+        <img src={Logo} />
         <div className="hiddenLinks">
           <Link to="/home">Home</Link>
           <Link to="/menu">Menu</Link>
